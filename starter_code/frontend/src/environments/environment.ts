840ec959c488b389c120664026b042f1e21c2426
anushka <anushka@anushkas-MBP.lan>
2021-05-05 21:03:44 +0530

    first commit

---
+++ starter_code/frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'dev-zz43b2ca.us.auth0.com', // the auth0 domain prefix
+    audience: 'drink', // the audience set for the auth0 app
+    clientId: 'JC0FkEUBKGvrSWhenZQJAOw5EnveLMEQ', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+  }
+};
